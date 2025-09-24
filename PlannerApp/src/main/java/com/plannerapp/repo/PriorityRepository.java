e38ba2a4b19f84363e1ec08e8edb43403be534a4
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-09-24 17:29:25 +0300

    PlannerApplication

---
+++ PlannerApp/src/main/java/com/plannerapp/repo/PriorityRepository.java
@@ -0,0 +1,15 @@
+package com.plannerapp.repo;
+
+import com.plannerapp.model.entity.Priority;
+import com.plannerapp.model.enums.PriorityName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface PriorityRepository extends JpaRepository<Priority, UUID> {
+
+    Priority findByName(PriorityName priority);
+}
