73501765b8f48650194a20cd20e48a36e6efdace
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-10-02 17:25:06 +0300

    PlannerApplication

---
+++ PlannerApp/src/main/java/com/plannerapp/repo/UserRepository.java
@@ -0,0 +1,16 @@
+package com.plannerapp.repo;
+
+import com.plannerapp.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.validation.constraints.Size;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    boolean existsByUsernameOrEmail(String username, String email);
+
+    User findByUsername(String username);
+}
