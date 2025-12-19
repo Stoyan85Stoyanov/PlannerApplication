f21248ac1b3317f7239995651a1eccc6f837a14a
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-12-19 12:54:34 +0200

    PlannerApplication

--- PlannerApp/src/main/java/com/plannerapp/init/PriorityInit.java
+++ PlannerApp/src/main/java/com/plannerapp/init/PriorityInit.java
@@ -26,7 +26,7 @@ public class PriorityInit implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
         long count = this.priorityRepository.count();
 
